a9c4cb3c960cdc66ed9df42c6797bdea379f0823
nasrinakon2024 <nasrinakon2024@gmail.com>
2026-06-09 18:22:39 +0600

    feat: use environment variables for firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "idea-vault-project-22cff.firebaseapp.com",
-  projectId: "idea-vault-project-22cff",
-  storageBucket: "idea-vault-project-22cff.firebasestorage.app",
-  messagingSenderId: "856838482059",
-  appId: "1:856838482059:web:99f2721672001372facc8e",
-  measurementId: "G-7XFEMXMB50"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
